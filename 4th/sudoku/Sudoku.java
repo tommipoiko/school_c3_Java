9fd9b7c7bdef7cbe76a92d94da3c1b98d7c41993
tommipoiko <tommi.poiko@gmail.com>
2022-01-28 17:09:39 +0200

    Sudoku valmis

--- 4th/sudoku/Sudoku.java
+++ 4th/sudoku/Sudoku.java
@@ -12,11 +12,11 @@ public class Sudoku {
     public Sudoku() {
         
         for (int y = 0; y < 9; y++) {
-            HashMap<Integer, String> x_row = new HashMap<>();
+            HashMap<Integer, String> row = new HashMap<>();
             for (int x = 0; x < 9; x++) {
-                x_row.put(y, " ");
+                row.put(x, " ");
             }
-            this.board.put(y, x_row);
+            this.board.put(y, row);
         }
     }
     
@@ -27,10 +27,10 @@ public class Sudoku {
                 "6", "7", "8", "9"));
         
         if (i > 8 | j > 8) {
-            System.out.format("Trying to access illegal cell (%d, %d)\n", i, j);
+            System.out.format("Trying to access illegal cell (%d, %d)!\n", i, j);
         } else if (!accepted.contains(String.valueOf(c))) {
             System.out.print("Trying to set illegal character ");
-            System.out.format("%s to cell (%d, %d)\n", String.valueOf(c), i, j);
+            System.out.format("%s to (%d, %d)!\n", String.valueOf(c), i, j);
         } else {
             for (var row : this.board.get(i).keySet()) {
                 if (row == j) {
@@ -44,51 +44,122 @@ public class Sudoku {
         
         int y = 0;
         int x = 0;
-        String duplicate;
-        HashSet<String> char_check = new HashSet<>();
+        int y_loc = 0;
+        int x_loc = 0;
         int lowestduplicate = 10;
-        int outcome = 0;
         boolean doubles = false;
         
         // Check rows
         while (y < 9) {
+            HashSet<String> char_check = new HashSet<>();
             while (x < 9) {
-                duplicate = String.valueOf(this.board.get(y).get(x));
+                String duplicate = this.board.get(y).get(x);
                 if (!" ".equals(duplicate)) {
                     if (!char_check.contains(duplicate)) {
                         char_check.add(duplicate);
                     } else {
                         doubles = true;
-                        outcome = 1;
                         if (Integer.parseInt(duplicate) < lowestduplicate) {
                             lowestduplicate = Integer.parseInt(duplicate);
+                            y_loc = y;
                         }
                     }
                 }
                 x++;
             }
+            if (doubles == true) {
+                System.out.format("Row %d has multiple %d's!\n", y_loc, lowestduplicate);
+                return false;
+            }
+            x = 0;
             y++;
         }
         
-        // Check columns
-
-        // Check small squares
+        y = 0;
+        x = 0;
+        lowestduplicate = 10;
         
-        // Act on findings
-        if (doubles == true) {
-            if (outcome == 1) {
-                // A row is fucked
-                // System.out.format("Row %d has multiple %d's!", y_location, lowestduplicate);
+        // Check columns
+        while (x < 9) {
+            HashSet<String> char_check = new HashSet<>();
+            while (y < 9) {
+                String duplicate = this.board.get(y).get(x);
+                if (!" ".equals(duplicate)) {
+                    if (!char_check.contains(duplicate)) {
+                        char_check.add(duplicate);
+                    } else {
+                        doubles = true;
+                        if (Integer.parseInt(duplicate) < lowestduplicate) {
+                            lowestduplicate = Integer.parseInt(duplicate);
+                            x_loc = x;
+                        }
+                    }
+                }
+                y++;
+            }
+            if (doubles == true) {
+                System.out.format("Column %d has multiple %d's!\n", x_loc, lowestduplicate);
                 return false;
-            } else if (outcome == 2) {
-                // A column is fucked
-                return false;
-            } else if (outcome == 3) {
-                // A box is fucked
-                return false;
+            }
+            y = 0;
+            x++;
+        }
+
+        y = 0;
+        x = 0;
+        y_loc = 0;
+        x_loc = 0;
+        lowestduplicate = 10;
+        HashSet<String> char_check = new HashSet<>();
+        
+        // Check the blocks
+        while (y < 9) {
+            while (x < 9) {
+                String duplicate = this.board.get(y).get(x);
+                if (!" ".equals(duplicate)) {
+                    if (!char_check.contains(duplicate)) {
+                        char_check.add(duplicate);
+                    } else {
+                        doubles = true;
+                        if (Integer.parseInt(duplicate) < lowestduplicate) {
+                            lowestduplicate = Integer.parseInt(duplicate);
+                            y_loc = y;
+                            x_loc = x;
+                        }
+                    }
+                }
+                
+                if ((y + 1) % 3 == 0 && (x + 1) % 3 == 0) {
+                    char_check.clear();
+                }
+                
+                if ((x + 1) % 3 != 0 && x != 8) {
+                    x++;
+                } else if ((x + 1) % 3 == 0 && (y + 1) % 3 != 0) {
+                    x -= 2;
+                    y++;
+                } else if ((x + 1) % 3 == 0 && (y + 1) % 3 == 0 && x != 8) {
+                    x++;
+                    y -= 2;
+                } else if (x == 8 && (y + 1) % 3 == 0 && y != 8) {
+                    x -= 8;
+                    y++;
+                } else {
+                    return true;
+                }
+                
+                // Tehtävässä virhe, koordinaatit ilmoitetaan mallissa ristikkäin
+                // muodossa (y, x). Tästä syystä outous seuraavassa pätkässä.
+                if (doubles == true) {
+                    int new_y_loc = y_loc - (y_loc % 3);
+                    int new_x_loc = x_loc - (x_loc % 3);
+                    System.out.format("Block at (%d, %d) has multiple %d's!\n", new_y_loc, new_x_loc, lowestduplicate);
+                    return false;
+                }
             }
         }
         
+        // If no illegalities were found
         return true;
     }
     

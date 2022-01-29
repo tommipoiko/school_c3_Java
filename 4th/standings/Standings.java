0b1f978eaf5d7ad3065715d290ba3e433b664e79
tommipoiko <tommi.poiko@gmail.com>
2022-01-29 11:44:23 +0200

    Standings all methods listed

--- 4th/standings/Standings.java
+++ 4th/standings/Standings.java
@@ -1,4 +1,4 @@
-// import here
+import java.util.ArrayList;
 
 public class Standings {
 
@@ -23,48 +23,78 @@ public class Standings {
         
         public int getWins() {
             
-            //
+            // Returns the amount of wins a team has
             
             return this.team_wins;
         }
         
         public int getTies() {
             
-            //
+            // Returns the amount of ties a team has
             
             return this.team_ties;
         }
         
         public int getLosses() {
             
-            //
+            // Returns the amount of losses a team has
             
             return this.team_losses;
         }
         
         public int getScored() {
             
-            //
+            // Returns the amount of scored goals a team has
             
             return this.team_scoredgoals;
         }
         
         public int getAllowed() {
             
-            //
+            // Returns the amount of allowed goals a team has
             
             return this.team_allowedgoals;
         }
         
         public int getPoints() {
             
-            //
+            // Returns the amount of ladder points a team has
             
             return this.team_points;
         }
     }
     
     public Standings() {
+        
+        //
+    }
+    
+    public Standings(String filename) {
+        
+        //
+    }
+    
+    public void readMatchData(String filename) {
+        
+        //
+    }
+    
+    public void addMatchResult(String teamNameA, int goalsA,
+            int goalsB, String teamNameB) {
+        
+        //
+    }
+    
+    public ArrayList<Team> getTeams() {
+        
+        //
+        
+        ArrayList<Team> teams = new ArrayList<>();
+        
+        return teams;
+    }
+    
+    public void printStandings() {
         
         //
     }

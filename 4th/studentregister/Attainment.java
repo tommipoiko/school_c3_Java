851f3bedd2df66e0022cdf6f2b25ce8fbfa484e0
tommipoiko <tommi.poiko@gmail.com>
2022-01-31 14:01:22 +0200

    Created the classes

---
+++ 4th/studentregister/Attainment.java
@@ -0,0 +1,8 @@
+// import here
+
+public class Attainment {
+
+    public static void main(String args[]){
+        // TODO code application logic here
+    }
+}
---
+++ 4th/studentregister/Course.java
@@ -0,0 +1,8 @@
+// import here
+
+public class Course {
+
+    public static void main(String args[]){
+        // TODO code application logic here
+    }
+}

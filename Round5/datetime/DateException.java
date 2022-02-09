e7ecbdaafcc4c59cec868756e66bc52555981dfb
tommipoiko <tommi.poiko@gmail.com>
2022-02-09 19:38:31 +0200

    Created the necessary files

---
+++ Round5/datetime/DateException.java
@@ -0,0 +1,8 @@
+// import here
+
+public class DateException {
+
+    public static void main(String args[]){
+        // TODO code application logic here
+    }
+}
---
+++ Round5/datetime/DateTime.java
@@ -0,0 +1,8 @@
+// import here
+
+public class DateTime {
+
+    public static void main(String args[]){
+        // TODO code application logic here
+    }
+}

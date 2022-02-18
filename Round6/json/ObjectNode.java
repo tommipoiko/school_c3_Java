4641ff77dc255646ad965f1a6fd65146c5ec3c19
tommipoiko <tommi.poiko@gmail.com>
2022-02-18 10:14:51 +0200

    Created the files for my next excercise

---
+++ Round6/json/ObjectNode.java
@@ -0,0 +1,8 @@
+// import here
+
+public class ObjectNode {
+
+    public static void main(String args[]){
+        // TODO code application logic here
+    }
+}
---
+++ Round6/json/ValueNode.java
@@ -0,0 +1,8 @@
+// import here
+
+public class ValueNode {
+
+    public static void main(String args[]){
+        // TODO code application logic here
+    }
+}

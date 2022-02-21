ba8ecd993209580b5b6f2958980fd7755e5bbb83
tommipoiko <tommi.poiko@gmail.com>
2022-02-21 20:15:08 +0200

    Created a neater print-method

--- Round6/json/Node.java
+++ Round6/json/Node.java
@@ -18,7 +18,68 @@ public abstract class Node {
   }
 
   public void printJson() {
-    throw new UnsupportedOperationException("printJson has not been implemented!");
+    StringBuilder sb = new StringBuilder();
+    sb.append("{").append(NL);
+    printJson(this, sb, 0);
+    sb.append("}").append(NL);
+    System.out.print(sb.toString());
+  }
+  
+  private void printJson(Node node, StringBuilder sb, int level) {
+    if(node.isObject()) {
+      ObjectNode objNode = (ObjectNode) node;
+      for(String name : objNode) {
+        for (int i = 0; i <= 2*level; i++) {
+          sb.append(" ");
+        }
+        sb.append("\"").append(name).append("\"").append(": ");
+        if (objNode.get(name).isObject()) {
+          sb.append("{\n");
+        } else if (objNode.get(name).isArray()) {
+          sb.append("[\n");
+        }
+        printJson(objNode.get(name), sb, level+1);
+        if (objNode.get(name).isObject()) {
+          for (int i = 0; i <= 2*level; i++) {
+            sb.append(" ");
+          }
+          sb.append("},\n");
+        } else if (objNode.get(name).isArray()) {
+          for (int i = 0; i <= 2*level; i++) {
+            sb.append(" ");
+          }
+          sb.append("],\n");
+        }
+      }
+    }
+    else if(node.isArray()) {
+      ArrayNode arrNode = (ArrayNode) node;
+      for(Node aNode : arrNode) {
+        for (int i = 0; i <= 2*level; i++) {
+          sb.append(" ");
+        }
+        sb.append("{\n");
+        printJson(aNode, sb, level+1);
+        for (int i = 0; i <= 2*level; i++) {
+          sb.append(" ");
+        }
+        sb.append("},\n");
+      }
+    }
+    else if(node.isValue()) {
+      ValueNode valNode = (ValueNode) node;
+      String valStr = "null";
+      if(valNode.isNumber()) {
+        valStr = numberToString(valNode.getNumber());
+      }
+      else if(valNode.isBoolean()) {
+        valStr = Boolean.toString(valNode.getBoolean());
+      }
+      else if(valNode.isString()) {
+        valStr = "\"" + valNode.getString() + "\"";
+      }
+      sb.append(String.format("%s%n", valStr));
+    }
   }
 
   private static final String NL = System.lineSeparator();

b2f89cf02078f71d2b9b70f3bee8383547fc94ec
tommipoiko <tommi.poiko@gmail.com>
2022-02-28 22:02:37 +0200

    Weekly excercizes completed

--- Round7/streams2/MovieAnalytics2.java
+++ Round7/streams2/MovieAnalytics2.java
@@ -3,7 +3,8 @@ import java.util.stream.Stream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.Map;
 
 public class MovieAnalytics2 {
 
@@ -37,16 +38,41 @@ public class MovieAnalytics2 {
 
     public void printCountByDirector(int n) {
 
-        int counter = 0;
+        Map<String, Long> map = this.movieList.stream()
+                .collect(Collectors.groupingBy(Movie::getDirector,
+                        Collectors.counting()));
+        Stream<Map.Entry<String, Long>> stream = mapToStream(map);
+        stream.sorted(Map.Entry.<String, Long>comparingByValue().reversed()
+                .thenComparing(Map.Entry.<String, Long>comparingByKey()))
+                .limit((long)n).forEach(s -> System.out
+                        .format("%s: %s movies%n", s.getKey(), s.getValue()));
     }
 
     public void printAverageDurationByGenre() {
 
-        //
+        Map<String, Double> map = this.movieList.stream()
+                .collect(Collectors.groupingBy(Movie::getGenre,
+                        Collectors.averagingInt(Movie::getDuration)));
+        Stream<Map.Entry<String, Double>> stream = mapToStream(map);
+        stream.sorted(Map.Entry.<String, Double>comparingByValue()
+                        .thenComparing(Map.Entry.<String, Double>comparingByKey()))
+                .forEach(s -> System.out
+                        .format("%s: %.2f%n", s.getKey(), s.getValue()));
     }
 
     public void printAverageScoreByGenre() {
 
-        //
+        Map<String, Double> map = this.movieList.stream()
+                .collect(Collectors.groupingBy(Movie::getGenre,
+                        Collectors.averagingDouble(Movie::getScore)));
+        Stream<Map.Entry<String, Double>> stream = mapToStream(map);
+        stream.sorted(Map.Entry.<String, Double>comparingByValue().reversed()
+                .thenComparing(Map.Entry.<String, Double>comparingByKey()))
+                .forEach(s -> System.out
+                        .format("%s: %.2f%n", s.getKey(), s.getValue()));
+    }
+
+    private static <K, V> Stream<Map.Entry<K, V>> mapToStream (Map<K, V> map) {
+        return map.entrySet().stream();
     }
 }

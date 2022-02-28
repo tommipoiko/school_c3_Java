caa006fea16eeabf6e4bfafeafd614c224e6c990
tommipoiko <tommi.poiko@gmail.com>
2022-02-28 20:56:01 +0200

    Done

--- Round7/streams/Movie.java
+++ Round7/streams/Movie.java
@@ -1,7 +1,47 @@
 // import here
 
 public class Movie {
-    public static void main(String[] args) {
-        //
+
+    // Private
+    String title;
+    int releaseYear;
+    int duration;
+    String genre;
+    double score;
+    String director;
+
+    // Public
+    public Movie(String title, int releaseYear, int duration, String genre, double score, String director) {
+
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.duration = duration;
+        this.genre = genre;
+        this.score = score;
+        this.director = director;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public String getDirector() {
+        return director;
     }
 }
--- Round7/streams/MovieAnalytics.java
+++ Round7/streams/MovieAnalytics.java
@@ -1,7 +1,101 @@
-// import here
+import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.function.Consumer;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Comparator;
 
 public class MovieAnalytics {
-    public static void main(String[] args) {
-        //
+
+    // Private
+    ArrayList<Movie> movieList;
+
+    // Public
+    public MovieAnalytics() {
+
+        this.movieList = new ArrayList<>();
+    }
+
+    public static Consumer<Movie> showInfo() {
+
+        Consumer<Movie> cm = new Consumer<Movie>() {
+            @Override
+            public void accept(Movie t) {
+                System.out.format("%s (By %s, %d)\n", t.getTitle(), t.getDirector(), t.getReleaseYear());
+            }
+        };
+
+        return cm;
+    }
+
+    public void populateWithData(String fileName) throws IOException {
+
+        try(var file = new BufferedReader(new FileReader(fileName))) {
+            String row = null;
+            while ((row = file.readLine()) != null) {
+                String[] file_row = row.split(";");
+                String title = file_row[0];
+                int releaseYear = Integer.parseInt(file_row[1]);
+                int duration = Integer.parseInt(file_row[2]);
+                String genre = file_row[3];
+                double score = Double.parseDouble(file_row[4]);
+                String director = file_row[5];
+
+                movieList.add(new Movie(title, releaseYear, duration, genre, score, director));
+            }
+        }
+    }
+
+    public Stream<Movie> moviesAfter(int year) {
+
+        ArrayList<Movie> tempList = new ArrayList<>();
+
+        for (var i : movieList) {
+            if (i.getReleaseYear() >= year) {
+                tempList.add(i);
+            }
+        }
+
+        return tempList.stream().sorted(Comparator.comparing(Movie::getReleaseYear).thenComparing(Movie::getTitle));
+    }
+
+    public Stream<Movie> moviesBefore(int year) {
+
+        ArrayList<Movie> tempList = new ArrayList<>();
+
+        for (var i : movieList) {
+            if (i.getReleaseYear() <= year) {
+                tempList.add(i);
+            }
+        }
+
+        return tempList.stream().sorted(Comparator.comparing(Movie::getReleaseYear).thenComparing(Movie::getTitle));
+    }
+
+    public Stream<Movie> moviesBetween(int yearA, int yearB) {
+
+        ArrayList<Movie> tempList = new ArrayList<>();
+
+        for (var i : movieList) {
+            if (i.getReleaseYear() >= yearA && i.getReleaseYear() <= yearB) {
+                tempList.add(i);
+            }
+        }
+
+        return tempList.stream().sorted(Comparator.comparing(Movie::getReleaseYear).thenComparing(Movie::getTitle));
+    }
+
+    public Stream<Movie> moviesByDirector(String director) {
+
+        ArrayList<Movie> tempList = new ArrayList<>();
+
+        for (var i : movieList) {
+            if (i.getDirector().equals(director)) {
+                tempList.add(i);
+            }
+        }
+
+        return tempList.stream().sorted(Comparator.comparing(Movie::getReleaseYear).thenComparing(Movie::getTitle));
     }
 }
